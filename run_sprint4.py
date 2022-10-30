84a1e643f529d2c0f0aabf8a2a27e44d5001c8eb
Ilan <bebetteryou2015@gmail.com>
2022-10-30 12:44:19 +0000

    added sprint4 stuff

---
+++ run_sprint4.py
@@ -0,0 +1,32 @@
+from nebula3_experts.experts.pipeline.api import PipelineApi, PipelineTask
+import yaml, os
+from typing import Tuple
+
+def find(d, tag):
+    if tag in d:
+        yield d[tag]
+    for k, v in d.items():
+        if isinstance(v, dict):
+            for i in find(v, tag):
+                yield i
+
+def test_pipeline_task(pipeline_id):
+    class MyTask(PipelineTask):
+        def process_movie(self, movie_id: str) -> Tuple[bool, str]:
+            print (f'handling movie: {movie_id}')
+            # task actual work
+            print(" Hello World. ")
+            return True, None
+        def get_name(self) -> str:
+            return "my-task"
+
+    pipeline = PipelineApi(None)
+    task = MyTask()
+    pipeline.handle_pipeline_task(task, pipeline_id, stop_on_failure=True)
+
+def test():
+    pipeline_id = os.getenv('PIPELINE_ID')
+    test_pipeline_task(pipeline_id)
+
+if __name__ == '__main__':
+    test()
